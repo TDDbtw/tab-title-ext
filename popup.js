57f54fb882a923c317bacefe2c280ee6ecb5d421
TDDbtw <basilp1197@gmail.com>
2025-11-27 13:08:26 +0530

    backend

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('getTitle').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const currentTab = tabs[0];
-    const title = currentTab.title || "No title found";
-
-    document.getElementById('title').textContent = title;
-  });
-});
